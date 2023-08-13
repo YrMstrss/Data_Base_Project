45318739f4f5a1c563f9e3ec2fb6655fd3afffea
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-08-13 15:30:05 +0300

    added docstrings for methods of BDManager class

--- src/BDM_class.py
+++ src/BDM_class.py
@@ -13,6 +13,10 @@ class BDManager:
         self.conn = psycopg2.connect(host=self.host, database=self.database, user=self.user, password=self.password)
 
     def get_companies_and_vacancies_count(self):
+        """
+        Get names of all employers and quantity of open vacancies
+        :return: none
+        """
         with self.conn.cursor() as cur:
             cur.execute("SELECT Название, Количество_открытых_вакансий FROM employers")
             rows = cur.fetchall()
@@ -20,6 +24,10 @@ class BDManager:
                 print(row)
 
     def get_all_vacancies(self):
+        """
+        Get all vacancies, url, data about salary and company name
+        :return: none
+        """
         with self.conn.cursor() as cur:
             cur.execute("""
             SELECT vacancies.Название, vacancies.Ссылка, Минимальная_зарплата, Максимальная_зарплата,
@@ -33,6 +41,10 @@ class BDManager:
                 print(row)
 
     def get_avg_salary(self):
+        """
+        Get the average min salary ignoring vacancies in which information about min salary is not specified
+        :return: none
+        """
         with self.conn.cursor() as cur:
             cur.execute("""
             SELECT AVG(Минимальная_зарплата) FROM vacancies
@@ -43,6 +55,10 @@ class BDManager:
             print(average_payment)
 
     def get_vacancies_with_higher_salary(self):
+        """
+        Get vacancies' data with min salary higher than average salary
+        :return: none
+        """
         with self.conn.cursor() as cur:
             cur.execute(f"""
             SELECT * FROM vacancies
@@ -54,6 +70,11 @@ class BDManager:
                 print(row)
 
     def get_vacancies_with_keyword(self):
+        """
+        Get vacancies containing keyword in name of vacancy. Default keyword 'python', it can be changed at
+        initialization of class's instance
+        :return: none
+        """
         with self.conn.cursor() as cur:
             cur.execute(f"""
             SELECT * FROM vacancies
