5c5176530db47f2f93769850b68841f334298384
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-08-13 13:34:12 +0300

    add __init__ method

--- src/BDM_class.py
+++ src/BDM_class.py
@@ -1,7 +1,14 @@
+import psycopg2
+
+
 class BDManager:
 
-    def __init__(self):
-        pass
+    def __init__(self, host='localhost', database='hh data base', user='postgres', password='qwaszxL1'):
+        self.host = host
+        self.database = database
+        self.user = user
+        self.password = password
+        self.conn = psycopg2.connect(host=self.host, database=self.database, user=self.user, password=self.password)
 
     def get_companies_and_vacancies_count(self):
         pass
